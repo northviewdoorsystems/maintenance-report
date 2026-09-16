6bc74bcfd00966a0726fc18571a37659ba084236
northviewdoorsystems <info@northviewdoorsystems.com>
2026-09-16 17:18:37 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
   and re-upload, so returning devices pick up the new version instead of
   serving a stale cached copy forever.
 */
-const CACHE_NAME = "nv-tuneup-v13";
+const CACHE_NAME = "nv-tuneup-v14";
 
 const SHELL_FILES = [
   "./",
